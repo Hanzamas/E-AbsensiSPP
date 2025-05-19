3432ba1c224508ba88dbf1497471354b5a5332cc
AhmadMnrl <serigalaberbulu465@gmail.com>
2025-05-19 21:45:33 +0700

    fix api

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -16,18 +16,17 @@ class ApiEndpoints {
   static const String updateProfile = '/users/profile/update';
   
   // Student endpoints
-  static const String getAttendanceDetail= '/attendance/detail';
+  static const String getAttendance = '/students/attendance';
   static const String getSPP = '/student/spp';
   static const String paySPP = '/student/spp/pay';
   static const String getSPPHistory = '/student/spp/history';
-  static const String updateStudent = '/students/update';
-  static const String getStudentDetail = '/students/detail';
   static const String getKelas = '/classes';
   static const String getStudentSchedule = '/students/schedule';
+  static const String getAttendanceDownload = '/attendance/download';
   
   // Admin endpoints
   static const String getStudents = '/admin/students';
-  static const String getAttendance = '/admin/attendance';
+  static const String getAdminAttendance = '/admin/attendance';
   static const String getAttendanceReport = '/admin/attendance/report';
   static const String getSPPReport = '/admin/spp/report';
   static const String addStudent = '/admin/students/add';
